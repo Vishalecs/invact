b0c405296beea377720f53eee9eacaf7a00a545f
Vishalecs <vishalsinghecs99@gmail.com>
2024-07-01 18:58:59 +0530

    Updated MovieList component with styled Rating component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -5,11 +5,17 @@ const Rating = ({ rating, onRate }) => {
   const stars = [1, 2, 3, 4, 5];
 
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       {stars.map((star) => (
         <span
           key={star}
-          style={{ cursor: 'pointer', color: star <= rating ? 'gold' : 'gray' }}
+          style={{
+            cursor: 'pointer',
+            fontSize: '24px', // Increase size of stars
+            color: star <= rating ? 'gold' : 'gray',
+            display: 'inline-block', // Ensure stars are inline
+            margin: '0 5px', // Add margin between stars for spacing
+          }}
           onClick={() => onRate(star)}
         >
           ★
